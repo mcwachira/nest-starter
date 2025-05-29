30e8c7b209326e8651bb380da6b0d12a0b4f213e
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-29 20:55:40 +0300

    insalled nestjs config packages for accessing enviromet varaibles and setup my config file to easily access the enviroment variables

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
   getHello(): string {
+    const enviromentVariable = this.configService.get<string>('enviroment');
+    console.log(`Test enviroment variable is :${enviromentVariable}`);
     return 'Hello World!';
   }
 }
---
+++ src/config/index.ts
@@ -0,0 +1,3 @@
+export default () => ({
+  enviroment: process.env.NODE_ENV || 'developemnt',
+});
